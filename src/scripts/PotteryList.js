58cf164d70370d8c3c076700c144cf8a5647cff1
herringvoices <nalherring@gmail.com>
2024-09-11 13:39:30 -0500

    Add article element to index.html. Add potteryList(). Output items to DOM.

---
+++ src/scripts/PotteryList.js
@@ -0,0 +1,15 @@
+import { usePottery } from "./PotteryCatalog.js";
+
+export const potteryList = () => {
+    let potHTML = ""
+    for (const item of usePottery()) {
+        potHTML += `<section class="pottery" id="pottery--1">
+  <h2 class="pottery__shape">${item.shape}</h2>
+  <div class="pottery__properties">
+    Item weighs ${item.weight} hectograms and is ${item.height} cm in height
+  </div>
+  <div class="pottery__price">Price is \$${item.price}</div>
+</section>`
+    }
+    return potHTML
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,17 @@
 import { makePottery } from "./PotteryWheel.js"
 import { firePottery } from "./Kiln.js"
 import { toSellOrNotToSell, usePottery } from "./PotteryCatalog.js"
+import { potteryList } from "./PotteryList.js"
+
+//Capture the potteryList element
+let potteryHTML = document.querySelector(".potteryList")
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery("mug", 1, 5, 1)
-let plate = makePottery("plate", 1.5, 1, 2)
-let bowl = makePottery("bowl", 1.75, 4.5, 3)
-let vase = makePottery("vase", 6, 18, 4)
-let dogBowl = makePottery("dog bowl", 2.5, 1.8, 5)
+let mug = makePottery("mug", 3, 5, 1)
+let plate = makePottery("plate", 4, 1, 2)
+let bowl = makePottery("bowl", 5, 4.5, 3)
+let vase = makePottery("vase", 10, 18, 4)
+let dogBowl = makePottery("dog bowl", 7, 1.8, 5)
 
 
 // Fire each piece of pottery in the kiln
@@ -19,21 +23,13 @@ firePottery(bowl, 2000)
 firePottery(vase, 2000)
 firePottery(dogBowl, 2000)
 
+// Determine which ones should be sold, and their price
 toSellOrNotToSell(mug)
 toSellOrNotToSell(plate)
 toSellOrNotToSell(bowl)
 toSellOrNotToSell(vase)
 toSellOrNotToSell(dogBowl)
 
-console.log(mug)
-console.log(plate)
-console.log(bowl)
-console.log(vase)
-console.log(dogBowl)
-console.log(usePottery())
-
-// Determine which ones should be sold, and their price
-
-
 // Invoke the component function that renders the HTML list
 
+potteryHTML.innerHTML = potteryList()
