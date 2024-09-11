92f7df8dc291c8605d1f2eee11b24c44dbf849e6
herringvoices <nalherring@gmail.com>
2024-09-11 11:34:26 -0500

    Create makePottery() function. Make 5 pieces of pottery

---
+++ src/scripts/PotteryWheel.js
@@ -0,0 +1,12 @@
+
+
+// returns an object to represent a piece of pottery with a shape, weight, height, and id offered as arguments
+const makePottery = (shape, weight, height, id) => {
+   const pot = {
+        shape: shape,
+        weight: weight,
+        height: height,
+        id: id
+    }
+return pot
+}
